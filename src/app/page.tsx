2e1f91d70a359f8c283541b524abb0e388d85d35
Cristo RPS <cristobits0101@gmail.com>
2024-11-06 19:24:41 +0000

    Refactoring folders and adding button for redirect to videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import HomeIntro from "@/components/content/home-intro";
+import HomeIntro from "@/components/articles/home-intro";
 
 export default function HomePage() {
   return (
     <HomeIntro
       title='Create a video watch player'
-      subtitle='Technical test performed by Cristo Rubén Pérez Suárez'
+      subtitle='Technical test performed by Cristo Pérez Suárez'
     />
   )
 }
---
+++ src/components/buttons/redirect-to-videos.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function redirect-to-videos() {
+  return (
+    <div>redirect-to-videos</div>
+  )
+}
