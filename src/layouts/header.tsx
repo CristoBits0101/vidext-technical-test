c11e0911cb01afe372ae0174e029841c1fd919a2
Cristo RPS <cristobits0101@gmail.com>
2024-11-06 17:06:23 +0000

    Change tailwind config and rename header component

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,8 +1,8 @@
 import ApplicationLogo from '@/components/marketing/application-logo'
 
-export default function header() {
+export default function Header() {
   return (
-    <header className='w-full h-8 sticky top-0 bg-red-200'>
+    <header className='w-fit h-8 sticky top-0 bg-red-200'>
       <ApplicationLogo />
     </header>
   )
