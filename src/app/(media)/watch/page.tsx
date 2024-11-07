3304e1977c68cabedb1bc85cda45a13459fd534f
Cristo RPS <cristobits0101@gmail.com>
2024-11-07 19:44:23 +0000

    Removing useless useEffect

--- src/app/(media)/watch/page.tsx
+++ src/app/(media)/watch/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { trpc } from '@/utils/trpc'
 import VideoPlayer from '@/components/video/video-player'
 import ViewCounter from '@/components/counters/view-counter'
